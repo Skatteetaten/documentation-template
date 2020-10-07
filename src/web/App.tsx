cc5b6ad22a728016141366b06adc1742737a8803
Espen Strømland <espen.stromland@skatteetaten.no>
2020-10-07 10:51:26 +0200

    fixed path for github

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -5,7 +5,7 @@ import { Layout } from './components/Layout';
 import { Home } from './routes/Home';
 
 export default () => (
-  <BrowserRouter>
+  <BrowserRouter basename="documentation-template">
     <Layout title="Documentation template" footer="2020">
       <Switch>
         <Route path="/" component={Home} />
