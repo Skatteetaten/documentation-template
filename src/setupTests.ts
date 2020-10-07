4cdfd4104771a02aa55cf6b7f39949a992328989
Espen Strømland <espen.stromland@skatteetaten.no>
2020-10-07 10:33:33 +0200

    Updates

--- src/setupTests.ts
+++
@@ -1,3 +0,0 @@
-// Brukes for testing av komponenter som bruker Designsystemet
-// https://skatteetaten.github.io/frontend-components/#section-kom-i-gang-for-utviklere
-import '@skatteetaten/frontend-components/utils/loadTheme';
--- src/web/components/SkeLink.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react';
-import { NavLink, NavLinkProps } from 'react-router-dom';
-
-type SkeLinkProps = NavLinkProps;
-
-export const SkeLink: FC<SkeLinkProps> = ({ children, ...rest }) => (
-  <NavLink {...rest}>{children}</NavLink>
-);
